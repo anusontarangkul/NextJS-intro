6fc0b9234ca18925563670fbf73caeddbae9756a
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-11-23 01:37:04 -0800

    render each ninja

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import styles from '../../styles/Ninjas.module.css'
 export const getStaticProps = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
@@ -13,11 +14,11 @@ const Ninjas = ({ ninjas }) => {
         <div>
             <h1>All ninjas</h1>
             {ninjas.map(ninja => (
-                <div key={ninja.id}>
+                <Link href={'/ninjas/' + ninja.id} key={ninja.id}>
                     <a className={styles.single}>
                         <h3>{ninja.name}</h3>
                     </a>
-                </div>
+                </Link>
             ))}
         </div>
     )
